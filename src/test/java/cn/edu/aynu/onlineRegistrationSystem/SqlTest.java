06993ed87cac39a13240c1fd3120b030e5b40798
Administrator <15993343299@163.com>
2019-11-03 11:25:49 +0800

    第一次测试了插入用户

--- src/test/java/cn/edu/aynu/onlineRegistrationSystem/SqlTest.java
+++ src/test/java/cn/edu/aynu/onlineRegistrationSystem/SqlTest.java
@@ -7,10 +7,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,34 +23,17 @@ import java.io.InputStream;
  * Author: Administrator
  * Create Data: 2019/11/3 0003
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration
 public class SqlTest {
-    SqlSession sqlSession=null;
-    SqlSessionFactory sqlSessionFactory=null;
-    @Before
-    public void beging() throws IOException {
-        sqlSessionFactory= SqlFactory.getSqlFactory();
-        sqlSession=sqlSessionFactory.openSession();
-    }
-    @After
-    public void after(){
-        sqlSession.commit();
-        sqlSession.close();
-    }
-
-
+    @Autowired
+    memInfoMapper memMapper=null;
     /**
      * 插入个人信息
      */
     @Test
     public void insertMemInfo(){
-        memInfoMapper mem=sqlSession.getMapper(memInfoMapper.class);
-        mem.insert(new memInfo(174804155,"岳阳","15993343299@163.com","M","123456"));
-    }
-}
-class SqlFactory{
-    public static SqlSessionFactory getSqlFactory() throws IOException {
-        InputStream in=Resources.getResourceAsStream("mapper/memInfoMapper.xml");
-        return new SqlSessionFactoryBuilder().build(in);
+        memMapper.insert(new memInfo(174804155,"岳阳","15993343299@163.com","M","123456"));
     }
 }
