2d999f9bd418273bc7283c54df28989e6d4a7df9
wyscoder <2900086967@qq.com>
2019-11-03 10:17:24 +0800

    修改

--- src/main/java/cn/edu/aynu/onlineRegistrationSystem/controller/HelloController.java
+++ src/main/java/cn/edu/aynu/onlineRegistrationSystem/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/Hello")
     public String hello(){
-        return "/123";
+        return "/signup";
     }
 }
