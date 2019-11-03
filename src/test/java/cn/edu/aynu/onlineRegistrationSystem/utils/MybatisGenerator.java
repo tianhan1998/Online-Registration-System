9ea0afd7b80aae3aa10805042dedb79a0c99dbe7
Administrator <15993343299@163.com>
2019-11-03 10:35:51 +0800

    mybatis逆向工程生成了dao和mapper

---
+++ src/test/java/cn/edu/aynu/onlineRegistrationSystem/utils/MybatisGenerator.java
@@ -0,0 +1,32 @@
+package cn.edu.aynu.onlineRegistrationSystem.utils;
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * company: www.abc.com
+ * Author: Administrator
+ * Create Data: 2019/11/3 0003
+ */
+public class MybatisGenerator {
+    @Test
+    public void mybatisGenerator() throws InterruptedException, SQLException, IOException, XMLParserException, InvalidConfigurationException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("D:\\Online-Registration-System\\src\\main\\resources\\MybatisGenerator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
