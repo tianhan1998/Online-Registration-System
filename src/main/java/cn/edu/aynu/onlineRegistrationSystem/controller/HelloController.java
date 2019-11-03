b22471cbe30d59935dafc0d0cb43d5382d299ea1
Administrator <15993343299@163.com>
2019-11-03 10:07:42 +0800

    我提交了一个bug

--- src/main/java/cn/edu/aynu/onlineRegistrationSystem/controller/HelloController.java
+++ src/main/java/cn/edu/aynu/onlineRegistrationSystem/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/Hello")
     public String hello(){
-        return "/signup";
+        return "/123";
     }
 }
