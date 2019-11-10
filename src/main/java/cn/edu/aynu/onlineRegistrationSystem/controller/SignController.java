9da52c3c4619ad8544516111330d48d7b8c11e02
tianhan <892265525@qq.com>
2019-11-10 21:29:28 +0800

    修改msg

--- src/main/java/cn/edu/aynu/onlineRegistrationSystem/controller/SignController.java
+++ src/main/java/cn/edu/aynu/onlineRegistrationSystem/controller/SignController.java
@@ -32,21 +32,21 @@ public class SignController {
      * @param memName 姓名
      * @param memPassowrd 密码
      * @param memEmail 安全邮箱
-     * @return json{code:200,meg:xxx,object:[]}
+     * @return json{code:200,msg:xxx,data:[]}
      */
     @RequestMapping(value = "/signUp",method = RequestMethod.POST)
     public JSONObject signUp(String memId, String memName, String memPassowrd, String memEmail, String memSex)  {
 
         memInfo memInfo = new memInfo(Integer.valueOf(memId),memName,memEmail,"M",memPassowrd);
-
+s
         if(service.singUp(memInfo)){
             String obj = JSONObject.toJSONString(memInfo);
             json.put("code",200);
-            json.put("meg","注册成功");
-            json.put("object",obj);
+            json.put("msg","注册成功");
+            json.put("data",obj);
         }else{
-            json.put("success","404");
-            json.put("meg","用户已存在！");
+            json.put("success",404);
+            json.put("msg","用户已存在！");
         }
 
         return json;
@@ -55,7 +55,7 @@ public class SignController {
     /***
      *注册ajax检测id是否重复
      * @param memId 检查id重复
-     * @return json{code:200,meg:"此学号可以注册"}
+     * @return json{code:200,msg:"此学号可以注册"}
      */
     @RequestMapping(value = "/signUp",method = RequestMethod.GET)
     public JSONObject checkId(Integer memId){
@@ -66,7 +66,7 @@ public class SignController {
      *post传入登录表单
      * @param id 前端学号
      * @param password 密码
-     * @return json{code:200,meg:登陆成功,object:登录用户对象}
+     * @return json{code:200,msg:登陆成功,data:登录用户对象}
      */
     @RequestMapping(value = "/signIn",method = RequestMethod.POST)
     public JSONObject signIn(String id,String password){
@@ -74,12 +74,12 @@ public class SignController {
         list=service.signIn(Integer.valueOf(id),password);
         if(list.size()!=0){
             String jsonstring=JSONArray.toJSONString(list.get(0));
-            json.put("object",jsonstring);
+            json.put("data",jsonstring);
             json.put("code",200);
-            json.put("meg","登陆成功");
+            json.put("msg","登陆成功");
         }else{
             json.put("success",404);
-            json.put("meg","学号不存在或密码错误");
+            json.put("msg","学号不存在或密码错误");
         }
         return json;
     }
